193426f3ae61c5bc14075cf563d823cd09719d73
Margaret Dorsey <margaretdorsey94@gmail.com>
2015-11-25 12:52:08 -0500

    adding FormMail form

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
 	<script> window.onload = function(){document.getElementsByName("contactnav")[0].setAttribute("id","current")}  </script>
 </head>
 <body>
-<?php include '../include/header.html'; ?>
+<?php include 'include/header.html'; ?>
 
 
 <article id="contact">
@@ -28,8 +28,49 @@
 	<a href="https://github.com/Abhalphiest"> <img src="media/index/github.png" title="My GitHub Account" alt="github" width="50" height="50" /> </a>
 	<a href="https://www.linkedin.com/in/margaretdorsey"> <img src="media/index/linkedin.png" alt="linkedin" title="My LinkedIn Profile" width="50" height="50"/> </a>
 	
+	<form method="post" action="http://people.rit.edu/med7068/formmail.php" name="ICEform">
+	<input type ="hidden" name="env_report" value="REMOTE_HOST,REMOTE_ADDR,HTTP_USER_AGENT,AUTH_TYPE,REMOTE_USER" />
+	<input type ="hidden" name="recipients" value ="med7068@rit.edu" />
+	<input type="hidden" name="required" value="EmailAddr:Your email address,FullName:Your name" />
+	<input type = "hidden" name="derive fields" value="email=EmailAddr, realname=FullName, subject=Subject" />
+	<input type = "hidden" name="mail_options" value="Exclude=email;realname" />
+	
+<table cellspacing="5%">
+	<tr>
+		<td valign="top">
+		<p> Name: </p>
+		</td>
+		<td><input type="text" name="FullName" /> </td>
+	</tr>
+	<tr>
+		<td valign="top">
+		<p> Email: </p>
+		</td>
+		<td><input type="text" name="EmailAddr" /> </td>
+	</tr>
+	<tr>
+        <td valign="top">
+        <p>Subject</p>
+        </td>
+        <td  valign="bottom"><input type="text" name="Subject" /> </td>
+        </td>
+    </tr>
+	<tr>
+        <td valign="top">
+        <p>Message</p>
+        </td>
+        <td ><textarea name="mesg" rows="10" cols="50"></textarea>
+        </td>
+    </tr>
+	<tr>
+        <td><input type="submit" value="Submit" /></td>
+        <td style="text-align:right;"><span style="font-size:60%;">Powered by Tectite <a href="http://www.tectite.com/" target="_blank">php form</a> (FormMail).</span>
+        </td>
+    </tr>
+	</table>
+	
 </article>
-<?php include '../include/footer.php'; ?>
+<?php include 'include/footer.php'; ?>
 </body>
 </html>
 
