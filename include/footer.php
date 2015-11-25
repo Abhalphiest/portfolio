fa633940146b2576477a7fab7b7333ec9cae2800
Margaret Dorsey <margaretdorsey94@gmail.com>
2015-11-25 12:35:23 -0500

    timezone format

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,3 @@
 <footer>
-</p>All images, html, css, javascript by me. This site uses jQuery and FormMail. <?php echo "Last updated " . date ("F d Y H:i:s.", getlastmod()); ?> </p>
+</p>All images, html, css, javascript by me. This site uses jQuery and FormMail. <?php echo "Last updated " . date ("l F jS, Y H:i:s e", getlastmod()); ?> </p>
 </footer>
