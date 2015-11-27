8b17bebf1d3a2ee6cec608314f7d58733f044ad8
Margaret Dorsey <margaretdorsey94@gmail.com>
2015-11-27 00:00:14 -0500

    .htaccess

---
+++ error.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />  <!-- for responsive design on devices that try to autoscale -->
+	<title> 404 Error - Page Not Found </title>
+</head>
+<body>
+<p> Oops, it looks like you ended up in the wrong spot! </p>
+</body>
+
+</html>
