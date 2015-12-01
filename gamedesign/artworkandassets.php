1e1094c0674c77249786832190117a759352937a
Margaret Dorsey <margaretdorsey94@gmail.com>
2015-12-01 12:53:22 -0500

    added script for lightboxes, still need to build the gallery

--- gamedesign/artworkandassets.php
+++ gamedesign/artworkandassets.php
@@ -8,6 +8,7 @@
 	<link rel="stylesheet" type="text/css" href="../css/gallery.css" />
 	<script> window.onload = function(){document.getElementsByName("gamenav")[0].setAttribute("id","current")}  </script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> <!-- include jQuery -->
+	<script src="../javascript/gallery.js"> </script> <!-- our gallery javascript -->
 </head>
 <body>
 
