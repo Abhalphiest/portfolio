c167a777b45e700a8cb1245252a7211035590b43
Margaret Dorsey <margaretdorsey94@gmail.com>
2015-12-06 01:01:01 -0500

    who even knows anymore

--- javascript/infinitescroll.js
+++ javascript/infinitescroll.js
@@ -2,7 +2,7 @@
 var posts =["renaissance.html", "brave.html","waterhose.html","markov.html"];
 var page=0;
 
-$(window).ready(function(ev) {
+window.onload = function(ev) {
    while ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && page < posts.length) {
         $(function () {
 				console.log("onload called");
