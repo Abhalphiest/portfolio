e52bdba86bf356bd648e7161984b97fa9aec089c
Margaret Dorsey <margaretdorsey94@gmail.com>
2015-12-06 21:52:48 -0500

    putting the php output somewhere more appropriate

--- contact.php
+++ contact.php
@@ -8,57 +8,7 @@
 	<script> window.onload = function(){document.getElementsByName("contactnav")[0].setAttribute("id","current")}  </script>
 </head>
 <body>
-<?php require 'PHPMailer-master/PHPMailerAutoload.php'; 
-date_default_timezone_set('Etc/UTC'); //neccessary for SMTP
-if(isset($_POST['submit']))
-{
-//Create a new PHPMailer instance
-$mail = new PHPMailer;
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-//Enable SMTP debugging
-// 0 = off (for production use)
-// 1 = client messages
-// 2 = client and server messages
-$mail->SMTPDebug = 2;
-//Ask for HTML-friendly debug output
-$mail->Debugoutput = 'html';
-//Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
-// use
-// $mail->Host = gethostbyname('smtp.gmail.com');
-// if your network does not support SMTP over IPv6
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 587;
-//Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = 'tls';
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "margaretdorsey94@gmail.com";
-//Password to use for SMTP authentication
-$mail->Password = "vzetod9n";
-//Set who the message is to be sent from
-$mail->setFrom($_POST['email'], $_POST['name']);
-//Set an alternative reply-to address
-$mail->addReplyTo($_POST['email'], $_POST['name']);
-//Set who the message is to be sent to
-$mail->addAddress($_POST['recipients'], 'Margaret Dorsey');
-//Set the subject line
-$mail->Subject = $_POST['Subject'];
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML($_POST['mesg'], dirname(__FILE__));
-//Replace the plain text body with one created manually
-$mail->AltBody = $_POST['mesg'];
-//send the message, check for errors
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-}
-}
-?> <!-- the alternative to formmail that I'm using -->
+
 <?php include 'include/header.html'; ?>
 
 
@@ -127,6 +77,62 @@ if (!$mail->send()) {
     </tr>
 	</table>
 </form>
+
+<div id="formoutput">
+<pre>
+<?php require 'PHPMailer-master/PHPMailerAutoload.php'; 
+date_default_timezone_set('Etc/UTC'); //neccessary for SMTP
+if(isset($_POST['submit']))
+{
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "margaretdorsey94@gmail.com";
+//Password to use for SMTP authentication
+$mail->Password = "vzetod9n";
+//Set who the message is to be sent from
+$mail->setFrom($_POST['email'], $_POST['name']);
+//Set an alternative reply-to address
+$mail->addReplyTo($_POST['email'], $_POST['name']);
+//Set who the message is to be sent to
+$mail->addAddress($_POST['recipients'], 'Margaret Dorsey');
+//Set the subject line
+$mail->Subject = $_POST['Subject'];
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML($_POST['mesg'], dirname(__FILE__));
+//Replace the plain text body with one created manually
+$mail->AltBody = $_POST['mesg'];
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+}
+?> <!-- the alternative to formmail that I'm using -->
+</pre>
+</div>
 </article>
 <?php include 'include/footer.php'; ?>
 </body>
