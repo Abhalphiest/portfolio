b1499e4f67d10ba8f61a5ecd2b36a44b3fb48a3a
Margaret Dorsey <margaretdorsey94@gmail.com>
2015-12-06 23:14:27 -0500

    updated notes page

--- notes.php
+++ notes.php
@@ -19,14 +19,17 @@ Fonts are Across the Road, Caviar Dreams and Rounded Elegance.
 single page application. I went through and cleaned up the CSS. </p>
 <h2> Requirements </h2>
 <table>
-<tr><td>Dynamic Page </td> <td>The blog is now an endlessly scrolling page.</td></tr>
-<tr><td>jQuery </td> <td>All image galleries are now jQuery image galleries, to cut down on the amount of page space spent on every project</td></tr>
-<tr><td>FormMail </td> <td>There's a mail form on the contact page.</td></tr>
-<tr><td>Server Side Includes</td> <td> I used php instead of direct SSIs, but the header and footer are included on the server side, and so is the date in the footer, among other things.</td></tr>
+<tr><td>Dynamic Page </td> <td>The blog is now an endlessly scrolling page. It pulls in and appends blog posts dynamically as you reach the end of the page.</td></tr>
+<tr><td>jQuery </td> <td>All image galleries are now jQuery image galleries,simply click on an image to start a lightbox gallery of those images. Transitions are used
+(slide down and slide up) on the navigation bar.</td></tr>
+<tr><td>Email Form </td> <td>There's a mail form on the contact page - it uses phpMailer and smtp through gmail, not formmail.</td></tr>
+<tr><td>Server Side Includes</td> <td> I used php instead of direct SSIs, but the header and footer are included on the server side, and so is the date in the footer, among other things,
+										like getting the http referer for the 404 page.</td></tr>
 <tr><td>.htaccess </td> <td> <a href="https://people.rit.edu/~med7068/gibsonAuth/auth.html"> Link to the Protected Directory</a>  <a href="boo.php"> Garbage Link to see the 404 </a></td></tr>
 </table>
 <h2> Above and Beyond </h2>
-<p> </p>
+<p> I added the linking system in the blog to pull up specific posts using jQuery, and learned a crap ton of php to be able to write the php for the mail form. I wrote the endlessly
+scrolling page and gallery scripts by hand instead of using an existing code base. </p>
 </article>
 <?php include 'include/footer.php'; ?>
 
