cd20abd5f5225d0bdd6c9caced2dfc7bed5f4c55
Margaret Dorsey <margaretdorsey94@gmail.com>
2015-12-06 23:07:00 -0500

    styled 404 page

--- error.php
+++ error.php
@@ -3,10 +3,20 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />  <!-- for responsive design on devices that try to autoscale -->
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
 	<title> 404 Error - Page Not Found </title>
+	<script>
+		window.onload = function(){
+			var whence= "<?php echo $_SERVER['HTTP_REFERER']; ?>";
+			document.getElementById("whence").href= whence;
+		}
+	</script>
 </head>
 <body>
-<p> Oops, it looks like you ended up in the wrong spot! </p>
+<article id="error">
+<p> <span>I don't think this is the page you were looking for..</span> <a id="whence" href="#"> Would you like to go back from whence you came? </a>
+<a href="index.php"> Or maybe go back to the front page? </a>
+</article>
 </body>
 
 </html>
