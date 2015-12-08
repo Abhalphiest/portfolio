c977b8c78f8da24af34d20cf679e60fc20d367ba
Margaret Dorsey <margaretdorsey94@gmail.com>
2015-12-07 22:45:55 -0500

    added js gallery to other picture pages

--- gamedesign/design.php
+++ gamedesign/design.php
@@ -7,6 +7,8 @@
 	<link rel="stylesheet" type="text/css" href="../css/styles.css" />
 	<link rel="stylesheet" type="text/css" href="../css/gallery.css" />
 	<script> window.onload = function(){document.getElementsByName("gamenav")[0].setAttribute("id","current")}  </script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> <!-- include jQuery -->
+	<script src="../javascript/gallery.js"> </script> <!-- our gallery javascript -->
 	
 </head>
 <body>
