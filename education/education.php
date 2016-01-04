0e70a89efaf8c1f427921267e790daf11b8031b9
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-01-04 14:09:23 -0500

    still refactoring..

---
+++ education/education.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />  <!-- for responsive design on devices that try to autoscale -->
+	<title> The Portfolio of Margaret Dorsey - Education</title>
+	<link rel="stylesheet" type="text/css" href="../css/styles.css" />
+	<link rel="stylesheet" type="text/css" href="../css/education.css" />
+	
+	
+	<script> window.onload = function(){document.getElementsByName("edunav")[0].setAttribute("id","current")}  </script>
+	<script src="../javascript/education.js"> </script>
+</head>
+<body>
+<?php include '../include/header.html'; ?>
+<ul id="edusubnav">
+<li class="subcurr">Computing</li>
+<li>Mathematics</li>
+<li>Game Design</li>
+<li>Coursework</li>
+</ul>
+<article id="main"> <!-- for the entire body of content -->
+
+</article>
+<?php include '../include/footer.php'; ?>
+</body>
+</html>
