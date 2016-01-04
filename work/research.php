fc2d89953aaa5e98ffa3d8d6e5ed2fabed1eaf93
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-01-03 22:40:27 -0500

    updated resume, will refactor whole site probably tomorrow

---
+++ work/research.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />  <!-- for responsive design on devices that try to autoscale -->
+	<title> The Portfolio of Margaret Dorsey - Research</title>
+	<link rel="stylesheet" type="text/css" href="../css/styles.css" />
+	<link rel="stylesheet" type="text/css" href="../css/project.css" />
+	<script> window.onload = function(){document.getElementsByName("mathnav")[0].setAttribute("id","current")}  </script>
+</head>
+<body>
+<?php include '../include/header.html'; ?>
+<article>
+<h1 id="mathtitle"> Research </h1>
+<section class="project">
+<h2> Lie Symmetries and Differential Equations </h2>
+<p> Research into the theoretical foundations of symmetry methods for differential equations, including Lie Symmetry Groups of ODEs, PDEs, and systems, infinitesimal generators and their prolongations, and discrete symmetries.
+Also investigated more advanced symmetry methods, such as variational symmetries, general (non-geometric) symmetries, and conservation laws. Currently investigating the application of these methods to various physical problems
+arising from the physical sciences.</p>
+<p> Research Mentor: Dr. Tamas Wiandt </p>
+</section>
+</article>
+<?php include '../include/footer.php'; ?>
+</body>
+</html>
