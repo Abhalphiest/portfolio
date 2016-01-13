602413ce4c07c8de3c4252f3c3cb89761ebfb95f
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-01-13 14:27:36 -0500

    css tweaks

--- javascript/section.js
+++ javascript/section.js
@@ -11,10 +11,7 @@ function setSubNav(n)
 			$(".subcurr").slideUp(800);
 			$(".subcurr").removeClass("subcurr");
 			$(id).addClass("subcurr");
-			
-			$(e).fadeOut();
 			$("#main").prepend($(id)).prepend($(e)); //move it to the top
-			$(e).fadeIn();
 			$(id).slideDown(800);
 		}
 		else
