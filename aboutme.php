9234bda7edf39196b574b338084a66b793807042
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-01-13 18:14:17 -0500

    threw something into the about me page

--- aboutme.php
+++ aboutme.php
@@ -12,13 +12,12 @@
 
 <?php include 'include/header.html'; ?>
 
-<main>
+<article>
 <h1> Welcome to my portfolio website!</h1>
 <p> My name is Margaret Dorsey, and I'm a second year dual major Game Design and Development and Computational Math student at the Rochester Institute of Technology.
-I am also following the Computer Science curriculum above and beyond my minor requirements, and completing a second minor in the Japanese language. Please feel free to look around
-and get to know me.
+I am also following the Computer Science curriculum above and beyond my minor requirements, and completing a second minor in the Japanese language. 
 </p>
-</main>
+</article>
 
 <?php include 'include/footer.php'; ?>
 
