85c297b1032e46a5286d8f204dbbb698456e7f45
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-01-13 18:06:47 -0500

    moving some things around

--- index.php
+++ index.php
@@ -17,15 +17,6 @@
 	
 </div>
 
-<main>
-<h1> Welcome to my portfolio website!</h1>
-<p> My name is Margaret Dorsey, and I'm a second year dual major Game Design and Development and Computational Math student at the Rochester Institute of Technology.
-I am also following the Computer Science curriculum above and beyond my minor requirements, and completing a second minor in the Japanese language. Please feel free to look around
-and get to know me.
-</p>
-
-<a href="notes.php"> Link to the notes page </a>
-</main>
 <?php include 'include/footer.php'; ?>
 
 </body>
