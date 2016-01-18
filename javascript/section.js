0e6a75d431cd81c78c83709ca07ed3b0ce7425f7
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-01-18 16:04:54 -0500

    testing

--- javascript/section.js
+++ javascript/section.js
@@ -1,12 +1,11 @@
 "use strict"
 	function sectionSwitch(id,e)
 	{
-		console.log("#"+$(".subcurr").attr("id")+"\n");
-		console.log(id+"\n");
 		if("#"+$(".subcurr").attr("id") != id)
 		{
 			$(".subcurr").slideUp(800);
 			$(".subcurr").removeClass("subcurr");
+			console.log($(id));
 			$(id).addClass("subcurr");
 			$("#main").prepend($(id)).prepend($(e)); //move it to the top
 			$(id).slideDown(800);
